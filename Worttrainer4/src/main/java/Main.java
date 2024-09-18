e283e825f705c89d1bca4ebd07fb4e3934d569b3
Dejan Rajic <78872776+Ubetterbisoft@users.noreply.github.com>
2024-09-18 20:43:53 +0200

    UML Rework

--- Worttrainer4/src/main/java/Main.java
+++ Worttrainer4/src/main/java/Main.java
@@ -1,10 +1,11 @@
+
 public class Main {
     /**
      * Mainloop for the programm
      * @param args
      */
     public static void main(String[]args){
-            System.out.println("Test");
+
 
     }
 }
