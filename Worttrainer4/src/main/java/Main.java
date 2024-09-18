2716758c31031fd032c2c01c388d586af232b142
Dejan Rajic <78872776+Ubetterbisoft@users.noreply.github.com>
2024-09-18 18:42:54 +0200

    First UML Concept

--- Worttrainer4/src/main/java/Main.java
+++ Worttrainer4/src/main/java/Main.java
@@ -1,5 +1,10 @@
 public class Main {
+    /**
+     * Mainloop for the programm
+     * @param args
+     */
     public static void main(String[]args){
             System.out.println("Test");
+
     }
 }
